8ede74d10fe28a5f6ea49f374ae27abcf3202f77
Paulo Marcos <132699727+Paulo-Marcos1@users.noreply.github.com>
2025-03-11 19:53:53 -0300

    Update Aluno.java

--- Exercicio 1/Aluno.java
+++ Exercicio 1/Aluno.java
@@ -4,11 +4,11 @@ import java.time.Period;
 public class Aluno {
 
     private String nome;
-    private LocalDate DatadeNascimento;
+    private LocalDate dataDeNascimento;
 
-    public Aluno(String nome, LocalDate DatadeNascimento){
+    public Aluno(String nome, LocalDate dataDeNascimento){
         this.nome = nome;
-        this.DatadeNascimento = DatadeNascimento;
+        this.dataDeNascimento = dataDeNascimento;
     }
 
     public String getNome(){
@@ -19,16 +19,16 @@ public class Aluno {
         this.nome = nome;
     }
 
-    public  LocalDate getDatadeNascimento(){
-        return DatadeNascimento;
+    public  LocalDate getdataDeNascimento(){
+        return dataDeNascimento;
     }
 
-    public void setDatadeNascimento(LocalDate datadeNascimento) {
-        this.DatadeNascimento = datadeNascimento;
+    public void setdataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
     }
 
     public int calcularidade(){
 
-        return Period.between(this.DatadeNascimento, LocalDate.now()).getYears();
+        return Period.between(this.dataDeNascimento, LocalDate.now()).getYears();
     }
 }
